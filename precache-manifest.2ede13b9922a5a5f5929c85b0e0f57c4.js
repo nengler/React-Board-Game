fbfeb2b9be2f65bdd8b18afdf212ced15fc12b99
Nathan Engler <englernathan@gmail.com>
2020-05-13 17:10:29 -0500

    Updates

---
+++ precache-manifest.2ede13b9922a5a5f5929c85b0e0f57c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74671c196f715560be88a273578bc2fb",
+    "url": "/React-Board-Game/index.html"
+  },
+  {
+    "revision": "c22cc8839d602e7e6e39",
+    "url": "/React-Board-Game/static/css/2.2f8cf4e0.chunk.css"
+  },
+  {
+    "revision": "43b23fd4cf67ac618c4c",
+    "url": "/React-Board-Game/static/css/main.2ab67446.chunk.css"
+  },
+  {
+    "revision": "c22cc8839d602e7e6e39",
+    "url": "/React-Board-Game/static/js/2.fb95b229.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Board-Game/static/js/2.fb95b229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b23fd4cf67ac618c4c",
+    "url": "/React-Board-Game/static/js/main.f4f14106.chunk.js"
+  },
+  {
+    "revision": "2240de5bd8a0411902c6",
+    "url": "/React-Board-Game/static/js/runtime-main.37ee9245.js"
+  },
+  {
+    "revision": "99ed922a5a8344b9d5e2986de88b1891",
+    "url": "/React-Board-Game/static/media/shield3.99ed922a.png"
+  },
+  {
+    "revision": "1e834a222a5aa87971559416d5256e5e",
+    "url": "/React-Board-Game/static/media/sword5.1e834a22.png"
+  }
+]);
